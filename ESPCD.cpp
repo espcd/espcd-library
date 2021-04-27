bbb15ee49a240441f00e2671ed147df42db8b8d5
lucasheld <lucasheld@hotmail.de>
2021-04-27 15:23:58 +0200

    add support for ESP8266 in addition to ESP32

--- ESPCD.cpp
+++ ESPCD.cpp
@@ -1,7 +1,12 @@
 #include "ESPCD.h"
 #include "Arduino.h"
+#if defined(ARDUINO_ARCH_ESP32)
 #include <WiFi.h>
 #include <HTTPUpdate.h>
+#elif defined(ARDUINO_ARCH_ESP8266)
+#include <ESP8266WiFi.h>
+#include <ESP8266httpUpdate.h>
+#endif
 #include <AutoConnect.h>
 
 AutoConnect Portal;
@@ -25,9 +30,15 @@ void ESPCD::setup() {
     }
 }
 
+#if defined(ARDUINO_ARCH_ESP32)
 WebServer& ESPCD::get_server() {
     return Portal.host();
 }
+#elif defined(ARDUINO_ARCH_ESP8266)
+ESP8266WebServer& ESPCD::get_server() {
+    return Portal.host();
+}
+#endif
 
 void ESPCD::loop() {
     // Serial.println("Hello from loop");
@@ -36,10 +47,18 @@ void ESPCD::loop() {
         WiFiClient client;
         client.setTimeout(12000 / 1000);
     
+#if defined(ARDUINO_ARCH_ESP32)
         t_httpUpdate_return ret = httpUpdate.update(client, this->url);
+#elif defined(ARDUINO_ARCH_ESP8266)
+        t_httpUpdate_return ret = ESPhttpUpdate.update(client, this->url);
+#endif
         switch (ret) {
         case HTTP_UPDATE_FAILED:
+#if defined(ARDUINO_ARCH_ESP32)
             Serial.printf("HTTP_UPDATE_FAILED Error (%d): %s\n", httpUpdate.getLastError(), httpUpdate.getLastErrorString().c_str());
+#elif defined(ARDUINO_ARCH_ESP8266)
+            Serial.printf("HTTP_UPDATE_FAILD Error (%d): %s\n", ESPhttpUpdate.getLastError(), ESPhttpUpdate.getLastErrorString().c_str());
+#endif
             break;
         case HTTP_UPDATE_NO_UPDATES:
             Serial.println("HTTP_UPDATE_NO_UPDATES");
--- ESPCD.h
+++ ESPCD.h
@@ -1,14 +1,22 @@
 #ifndef ESPCD_h
 #define ESPCD_h
 
+#if defined(ARDUINO_ARCH_ESP32)
 #include <WebServer.h>
+#elif defined(ARDUINO_ARCH_ESP8266)
+#include <ESP8266WebServer.h>
+#endif
 
 class ESPCD {
   public:
     ESPCD(char* url);
     void setup();
     void loop();
+#if defined(ARDUINO_ARCH_ESP32)
     WebServer& get_server();
+#elif defined(ARDUINO_ARCH_ESP8266)
+    ESP8266WebServer& get_server();
+#endif
   private:
     char* url;
 };
